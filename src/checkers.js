bc61aa2b3446e8183878424212a690e8d05e5b13
Michael Bashour <michael.s.bashour@gmail.com>
2014-12-23 17:22:01 -0600

    Add move prompt and getMove method.

--- src/checkers.js
+++ src/checkers.js
@@ -15,9 +15,9 @@ var resetBoard = function () {
   currentPlayer = 'wht'
 };
 
-var makeMove = function(row1, col1, row2, col2) {
-  board[charToNum[row1]][col1] = " X "
-  board[charToNum[row2]][col2] = currentPlayer
+var makeMove = function(moveObj) {
+  board[charToNum[moveObj['r1']]][moveObj['c1']] = " X "
+  board[charToNum[moveObj['r2']]][moveObj['c2']] = currentPlayer
   if(currentPlayer === "red"){
     currentPlayer = "wht"}
   else
@@ -25,4 +25,30 @@ var makeMove = function(row1, col1, row2, col2) {
     currentPlayer = "red"
   }
   displayBoard()
+}
+
+var attemptMove = function(row1, col1, row2, col2){
+  var errorCount = 0
+  if(errorCount === 0){
+    makeMove(row1, col1, row2, col2)
+  }
+}
+
+var removePiece = function(){
+
+}
+
+var getMove = function(){
+  var movePrompt = prompt("Please enter your move in format: R1, C1, R2, C2")
+  var moveArray = movePrompt.split(",")
+  var moveObj = {r1: moveArray[0], c1: moveArray[1], r2: moveArray[2], c2: moveArray[3]}
+  makeMove(moveObj)
+
+}
+
+var play = function(){
+  resetBoard()
+  displayBoard()
+  getMove()
+  // attemptMove()
 }
