6b10ca899b96f86ca5b304a186efe30553995bc5
Michael Bashour <michael.s.bashour@gmail.com>
2014-12-23 17:40:41 -0600

    Add some testing in attemptMove method to filter illegal moves.

--- src/checkers.js
+++ src/checkers.js
@@ -27,10 +27,36 @@ var makeMove = function(moveObj) {
   displayBoard()
 }
 
-var attemptMove = function(row1, col1, row2, col2){
+var attemptMove = function(moveObj){
   var errorCount = 0
+  // test moving only one row at a time
+  if(Math.abs(charToNum[moveObj['r1']] - charToNum[moveObj['r2']]) !== 1){
+    errorCount += 1
+    alert("Cannot move more than one row at a time")
+  }
+  // test moving only one column at a time
+  if(Math.abs(moveObj['c1'] - moveObj['c2']) != 1){
+    errorCount += 1
+    alert("Cannot move more than one colume at a time")
+  }
+
+  // test not moving off the board
+  // if(moveObj['c2'] > 8 || moveObj['c2'] < 0 || charToNum[moveObj['r2']] > 8 || charToNum[moveObj['r2']] < 8) {
+  //   errorCount += 1
+  //   alert("Don't move off the board")
+  // }
+  // test not moving opponent's piece
+  if(board[charToNum[moveObj['r1']]][moveObj['c1']] != currentPlayer){
+    errorCount += 1 
+    alert("It's not your turn.")
+  }
+  // test not moving a nonexistent piece
+
+
+  
+  
   if(errorCount === 0){
-    makeMove(row1, col1, row2, col2)
+    makeMove(moveObj)
   }
 }
 
@@ -42,7 +68,7 @@ var getMove = function(){
   var movePrompt = prompt("Please enter your move in format: R1, C1, R2, C2")
   var moveArray = movePrompt.split(",")
   var moveObj = {r1: moveArray[0], c1: moveArray[1], r2: moveArray[2], c2: moveArray[3]}
-  makeMove(moveObj)
+  attemptMove(moveObj)
 
 }
 
